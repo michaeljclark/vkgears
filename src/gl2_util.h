04dd36b38f39b80f7c4095792afcdf5cc4b9d168
Michael Clark <michaeljclark@mac.com>
2020-11-23 14:26:30 +1300

    gl2_util: add license and add r,g,b,a fields to vecNf unions

--- src/gl2_util.h
+++ src/gl2_util.h
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2020 Michael Clark <michaeljclark@mac.com>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
 #pragma once
 
 /*
@@ -6,9 +22,9 @@
 
 typedef unsigned uint;
 
-typedef union { float vec[2]; struct { float x, y;       } m; } vec2f;
-typedef union { float vec[3]; struct { float x, y, z;    } m; } vec3f;
-typedef union { float vec[4]; struct { float x, y, z, w; } m; } vec4f;
+typedef union { float vec[2]; struct { float x, y;       }; struct { float r, g;       }; } vec2f;
+typedef union { float vec[3]; struct { float x, y, z;    }; struct { float r, g, b;    }; } vec3f;
+typedef union { float vec[4]; struct { float x, y, z, w; }; struct { float r, g, b, a; }; } vec4f;
 
 typedef struct
 {
@@ -289,8 +305,8 @@ found:
  * shader utilties
  */
 
-static buffer load_file(const char *filename) 
-{ 
+static buffer load_file(const char *filename)
+{
     FILE *f;
     struct stat statbuf;
     char *buf;
@@ -339,13 +355,13 @@ static GLuint compile_shader(GLenum type, const char *filename)
         printf("shader compile log: %s\n", logbuf);
         free(logbuf);
     }
-    
+
     glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
     if (status == GL_FALSE) {
         printf("failed to compile shader: %s\n", filename);
         exit(1);
     }
-    
+
     return shader;
 }
 
